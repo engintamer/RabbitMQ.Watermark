19e9b14a3cfb68b3442321187bd721ba64b72855
Cemree <cemre1arslan@gmail.com>
2022-07-26 14:58:46 +0300

    Add project files.

---
+++ RabbitMQ.Web.Watermark/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQ.Web.Watermark.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
